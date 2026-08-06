726922fe7b57814e131051c1367b074e378baa63
Kexin Zhu <kzhu0029@student.monash.edu>
2026-08-06 18:20:39 +1000

    Add CORS support

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 
 const routesRouter = require("./routes/routes");
 const pedestrianRouter = require("./routes/pedestrian");
@@ -7,19 +8,28 @@ const refugeRouter = require("./routes/refuges");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+// Allow requests from the frontend
+app.use(cors());
 
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("FIT5120 Backend is running!");
 });
 
-
 app.use("/api/routes", routesRouter);
 app.use("/api/pedestrian", pedestrianRouter);
 app.use("/api/refuges", refugeRouter);
 
+// Simple error handler
+app.use((err, req, res, next) => {
+  console.error(err);
+
+  res.status(500).json({
+    error: "Internal server error",
+  });
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
